1eac4d3c089bf8626e27b5c98fd3fd53be459ffe
ethan6938 <en.hurwitz@gmail.com>
2025-06-02 12:17:35 +0200

    Update

--- chapter1/src/App.jsx
+++ chapter1/src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1>Learn React Hooks</h1>
+      <h1>🚀 Learn React Hooks</h1>
+      <p>Hooks make React easier.</p>
     </div>
   )
 }
